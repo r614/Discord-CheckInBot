e876ee9d0ab6b93d3ffb9fc713c84671ebf94fa7
Mike <mikeyue97@gmail.com>
2018-06-26 13:39:21 -0700

    huehuehue

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ TOKEN = ''
 channel_ID = 460312060446638083
 server_id = 460312060446638081
 team_list = [] #team_list is a list that contains lists. Each sublist contains two string elements, one for team number and one for team name
+role_list = [] #list of role objects
+channel_list = [] #list of newly created channels
+global server
 
 print(discord.__version__)
 with open('Discord_token.txt', 'r') as token_file:
@@ -20,6 +23,7 @@ with open("teams.txt") as textInfo:
 
 
 client = discord.Client()
+
 bot = commands.Bot(command_prefix='.')
 """Important - Assign teams as shown in the supporting file. Use ''-'' instead of spaces in team names, and keep one space between team number and team name """
 
@@ -43,16 +47,44 @@ async def on_message(message):
 			await bot.create_channel(ctx.message.server, name = role_name, type=discord.ChannelType.voice)
 		print('Done')
 	if message.content == '.test1':
-		server = message.channel.server
-		await client.create_channel(server, 'Test Voice Channel', type = discord.ChannelType.voice)
-		await client.send_message(message.channel, 'Voice Channel Created')
+		for team in team_list:
+			new_channel = await client.create_channel(server, team[1], type = discord.ChannelType.voice)
+			await client.edit_channel(new_channel, user_limit = 4)
+			channel_list.append(new_channel)
+			new_role = await client.create_role(server, name = team[1])
+			role_list.append(new_role)
+			overwrite = discord.PermissionOverwrite()
+			overwrite.connect = True
+			overwrite.speak = True
+			await client.edit_channel_permissions(new_channel, new_role, overwrite)
+
+		await client.send_message(message.channel, 'Voice Channels Created')
+	if message.content == ".clear":
+		logs = client.logs_from(message.channel)
+		async for msg in logs:
+			await client.delete_message(msg)
+	if message.content == ".permissiontest":
+		for role in server.roles:
+			print (role.name)
+		channel = client.get_channel(str(461255128788238356))
+	if message.content == ".delete_channels":
+		for channel in channel_list:
+			await client.delete_channel(channel)
+		for role in role_list:
+			await client.delete_role(server, role)
+
 
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+	global server
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
+	server = client.get_server(str(460312060446638081))
+	print(server)
+
+
 
 client.run(TOKEN)
