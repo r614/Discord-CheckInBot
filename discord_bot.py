135eeb28a8a8d3d02b859f05c43c0fdd0ee7ad03
r614 <35357371+r614@users.noreply.github.com>
2018-06-26 12:28:10 +0530

    Added .ready functionality
    Added team's .ready functionality and updated teams.txt file to add another column for the function array.

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import asyncio
 
 
 TOKEN = ''
@@ -16,14 +17,14 @@ bot = commands.Bot(command_prefix='.')
 async def on_ready():
     with open("teams.txt") as textInfo:
         team_list = [line.split() for line in textInfo]
-    await print(team_list)
+        print(team_list)
     await bot.say('Team List Loaded')
 
 """ setup reads the name of a team, creates a role and channel in the discord for that team. It does this for all the teams in the list. Also prints the team list with numbers in order."""
 
 @bot.command(pass_context = True)
 async def setup(ctx,message,team_list):
-    for index in range(len(team_list)):
+    for index in range(len(team_list[0])):
         role_name = team_list[index][1]
         await bot.create_role(ctx.message.server, name = role_name)
         await bot.send_message(ctx.message.channel, str(team_list[index][0] + ' ' + team_list[index][1]))
@@ -36,6 +37,21 @@ async def im(ctx,member, number, team_list):
     team_name = team_list[number-1][1]
     role = discord.utils.get(member.server.roles, name = team_name)
     await bot.add_roles(member, role)
+
+"This assumes that the user only has 2 roles on the server - @everyone and the assigned team role. The first element of member.roles is always @everyone hence the 2nd element is used."
+@bot.command(pass_context = True)
+async def ready(ctx, member, team_list):
+    member_role_list = member.roles
+    team_name = member_role_list[1]
+    flag = False
+    for index in range(len(team_list[0])):
+        if team_name == team_list[index][1]:
+            flag = True
+            team_list[index][2] = 1
+            bot.say('Team' + ' ' + str(team_name) + ' '+ 'is now ready!')
+            break
+        if flag == False:
+            bot.say('Error readying up, ask a moderator for info.')
 
 
 bot.run(TOKEN)
