2d1b0cc334f54eb454aa84b198e9c3cfbb86f26e
r614 <35357371+r614@users.noreply.github.com>
2018-06-27 23:18:05 +0530

    Add files via upload

--- discord_bot.py
+++ discord_bot.py
@@ -4,54 +4,60 @@ import os
 import asyncio
 
 
-TOKEN = ''
+TOKEN = 'NDYwNDIyNjg2OTUxMjExMDMw.DhQUSQ.YVgN_NI0GMZ--BVBm0AkmZN9VnQ'
 channel_ID = 460422215851048992
 
+team_list = []
+channel_list = []
+role_list = []
+global server
+global team_list
 
-bot = commands.Bot(command_prefix='.')
+
+bot = commands.Bot(command_prefix=',')
+client = discord.Client()
+
 """Important - Assign teams as shown in the supporting file. Use ''-'' instead of spaces in team names, and keep one space between team number and team name """
 
 """ on_ready reads the txt file into a 2D array team_list and prints it out into the console. """
 
-@bot.event
+@client.event
 async def on_ready():
     with open("teams.txt") as textInfo:
         team_list = [line.split() for line in textInfo]
         print(team_list)
-    await bot.say('Team List Loaded')
 
-""" setup reads the name of a team, creates a role and channel in the discord for that team. It does this for all the teams in the list. Also prints the team list with numbers in order."""
+@client.event
+async def on_message(message):
+    if message.content == '.setup':
+        for index in range(len(team_list[0])):
+            role_name = team_list[index][1]
+            server = message.channel.server
+            await client.create_role(ctx.member.server, name = role_name)
+            await client.send_message(message.channel, str(team_list[index][0] + ' ' + team_list[index][1]))
+            await client.create_channel(server, name = role_name, type=discord.ChannelType.voice)
+    if message.content == '.ready':
+        member_role_list = member.roles
+        team_name = member_role_list[1]
+        flag = False
+        for index in range(len(team_list[0])):
+            if team_name == team_list[index][1]:
+                flag = True
+                team_list[index][2] = true
+                client.say('Team' + ' ' + str(team_name) + ' '+ 'is now ready!')
+                print('Team' + ' ' + str(team_name) + ' '+ 'is now ready!')
+                break
+            if flag == False:
+                client.say('Error readying up, ask a moderator for info.')
 
-@bot.command(pass_context = True)
-async def setup(ctx,message,team_list):
-    for index in range(len(team_list[0])):
-        role_name = team_list[index][1]
-        await bot.create_role(ctx.message.server, name = role_name)
-        await bot.send_message(ctx.message.channel, str(team_list[index][0] + ' ' + team_list[index][1]))
-        await bot.create_channel(ctx.message.server, name = role_name, type=discord.ChannelType.voice)
+    if message.content == ".clear":
+		logs = client.logs_from(message.channel)
+        async for msg in logs:
+			await client.delete_message(msg)
 
-""" im takes the member name and team number as input and assigns the member to the role corresponding to the team number given in setup."""
-
-@bot.command(pass_context = True)
-async def im(ctx,member, number, team_list):
-    team_name = team_list[number-1][1]
-    role = discord.utils.get(member.server.roles, name = team_name)
-    await bot.add_roles(member, role)
-
-"This assumes that the user only has 2 roles on the server - @everyone and the assigned team role. The first element of member.roles is always @everyone hence the 2nd element is used."
-@bot.command(pass_context = True)
-async def ready(ctx, member, team_list):
-    member_role_list = member.roles
-    team_name = member_role_list[1]
-    flag = False
-    for index in range(len(team_list[0])):
-        if team_name == team_list[index][1]:
-            flag = True
-            team_list[index][2] = 1
-            bot.say('Team' + ' ' + str(team_name) + ' '+ 'is now ready!')
-            break
-        if flag == False:
-            bot.say('Error readying up, ask a moderator for info.')
-
+    if message.content == '.im':
+        team_name = team_list[number-1][1]
+        role = discord.utils.get(member.server.roles, name = team_name)
+        await client.add_roles(member, role)
 
 bot.run(TOKEN)
